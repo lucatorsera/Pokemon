207e7bba549f8f718a6609b5149d872aaf987f52
thiagokg <thiagokg525@gmail.com>
2016-04-28 14:34:53 -0300

    Update Evento.java

--- Evento.java
+++ Evento.java
@@ -1,13 +1,18 @@
-
 abstract public class Evento {
 	private int prioridade;
 	private boolean valido;
+	public Evento(int p){
+		prioridade = p;
+	}
 	public void setPrioridade(int p){
 		prioridade = p;
+	}
+	public int getPrioridade(){
+		return prioridade;
 	}
 	public boolean ehValido(){
 		return valido;
 	}
-	abstract public void action(Treinador ativo, Treinador oponent, int qual);
+	abstract public void action(Treinador ativo, Treinador oponente);
 	abstract public String description(Treinador ativo);
 }
