fe9ce5607e0b6f2f2220a40a3d7f6dc10c052f0e
thiagokg <thiagokg525@gmail.com>
2016-04-28 14:37:28 -0300

    Update Treinador.java

--- Treinador.java
+++ Treinador.java
@@ -1,15 +1,24 @@
-
 public class Treinador {
 	private String nome;
 	private Pokemon[] pokemons;
+	public Treinador(String name, Pokemon[] mons){
+		nome = name;
+		pokemons = mons;
+	}
 	public String getNome(){
 		return nome;
+	}
+	public int getNpokes(){
+		return pokemons.length;
 	}
 	public Pokemon getPokemon(int i){
 		if(i<=6)
 			return pokemons[i];
 		else
 			return null;
+	}
+	public Pokemon pokemonAtivo(){
+		return pokemons[0];
 	}
 	public void trocarPokemon(int i){
 		Pokemon aux;
@@ -18,10 +27,12 @@ public class Treinador {
 		pokemons[i] = aux;
 	}
 	//
-	private static int[] Ash1 = { 1, 2 };
-	private static int[] Ash2 = {};
-	private static int[] Gary1 = {};
-	private static int[] Gary2 = {};
+	private static int[] Ash1 = {0, 1, 2, 2, 1, 1, 1, 2, 1};
+	private static int[] Ash2 = {0, 1, 2, 1, 1, 1, 2, 2, 0};
+	private static int[] Gary1 = {0, 2, 1, 1, 2, 2, 1, 1};
+	private static int[] Gary2 = {0, 1, 0, 1, 2, 1, 0, 3};
+	private static int[] Red1 = {1, 3, 3, 3, 3, 3, 3, 3, 2, 3, 3, 3, 3, 3, 3, 3, 3};
+	private static int[] Red2 = {0, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1};
 	static public int[] getAsh(int i){
 		if(i==1)
 			return Ash1;
@@ -35,6 +46,14 @@ public class Treinador {
 			return Gary1;
 		if(i==2)
 			return Gary2;
+		else
+			return null;
+	}
+	static public int[] getRed(int i){
+		if(i==1)
+			return Red1;
+		if(i==2)
+			return Red2;
 		else
 			return null;
 	}
