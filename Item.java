603cfdd9a04ba6090a26bc2c5b8d32f3906e28bb
thiagokg <thiagokg525@gmail.com>
2016-04-28 14:36:02 -0300

    Update Item.java

--- Item.java
+++ Item.java
@@ -1,20 +1,52 @@
-
 public class Item extends Evento{
-	int DiferencaHP;
-	public void action(Pokemon ativo){
-		DiferencaHP = ativo.getHPmax() -ativo.getHP();
-		ativo.tomarDano(-50);
+	private int DiferencaHP;
+	private int qual; //0 = item de cura, qual!=0 sao as pokebolas
+	private boolean pegou;
+	public Item(int q){
+		super(3);
+		qual = q;
 	}
-	public String description(Pokemon ativo, Treinador player){
-		int healEfetivo;
-		if(DiferencaHP>=50)
-			healEfetivo = 50;
-		else
-			healEfetivo = DiferencaHP;
+	public void action(Treinador ativo, Treinador oponente){
+		if(qual==0){
+			DiferencaHP = ativo.pokemonAtivo().getHPmax() -ativo.pokemonAtivo().getHP();
+			ativo.pokemonAtivo().tomarDano(-50);
+		}
+		else{
+			double catchRate = 1.0 - (double)oponente.pokemonAtivo().getHP()/oponente.pokemonAtivo().getHPmax();
+			double rand = Math.random();
+			if(qual==2) //Great Ball
+				rand *= 1.5;
+			if(rand<catchRate){
+				oponente.pokemonAtivo().tomarDano(999999);
+				pegou = true;
+			}
+			else
+				pegou = false;
+		}
+	}
+	public String description(Treinador ativo){
 		String res = new String();
-		res = player.getNome() + " usou uma poção de cura! " + ativo.getNome + " curou-se em " + healEfetivo + "!";
-		if(ativo.getHP()==ativo.getHPmax())
-			res+= " " + ativo.getNome() + " está completamente curado.";
+		if(qual==0){
+			int healEfetivo;
+			if(DiferencaHP>=50)
+				healEfetivo = 50;
+			else
+				healEfetivo = DiferencaHP;
+			res = ativo.getNome() + " usou uma poÃ§Ã£o de cura! " + ativo.pokemonAtivo().getNome() + " curou-se em " + healEfetivo + "!";
+			if(ativo.pokemonAtivo().getHP()==ativo.pokemonAtivo().getHPmax())
+				res+= " " + ativo.pokemonAtivo().getNome() + " estÃ¡ completamente curado.";
+		}
+		else{
+			res += ativo.getNome() + " usou uma ";
+			if(qual==1)
+				res += "Pokebola.\n";
+			else
+				res += "Great Ball.\n";
+			if(pegou)
+				res += "O pokemon foi capturado!\n";
+			else
+				res += "Mas o pokemon conseguiu escapar!";
+		}
 		return res;
 	}
 }
